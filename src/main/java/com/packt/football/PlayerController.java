87d3087f1c7c63487eb0ac8ac297d7de3a292b25
Matheus da Silva <matheussimoesdasilva2002@gmail.com>
2024-11-01 14:14:32 -0300

    Basic HTTP requests

--- src/main/java/com/packt/football/PlayerController.java
+++ src/main/java/com/packt/football/PlayerController.java
@@ -1,4 +1,4 @@
-package com.pack.football;
+package com.packt.football;
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 
@@ -8,24 +8,25 @@ public class PlayerController {
 		public List<String> listPlayers() {
 				return List.of("Ivana ANDRES", "Alexia PUTELLAS");
 		}
+	
+		@PostMapping
+		public String createPlayer(@RequestBody String name) {
+				return "Player " + name + " created";
+		}
+
+		@GetMapping("/{name}")
+		public String readPlayer(@PathVariable String name) {
+				return name;
+		}
+
+		@DeleteMapping("/{name}")
+		public String deletePlayer(@PathVariable String name) {
+				return "Player " + name + " deleted";
+		}
+
+		@PutMapping("/{name}")
+		public String updatePlayer(@PathVariable String name, @RequestBody String newName) {
+				return "Player " + name + " updated to " + newName;
+		}
 }
 
-@PostMapping
-public String createPlayer(@RequestBody String name) {
-		return "Player " + name + " created";
-}
-
-@GetMapping("/{name}")
-public String readPlayer(@PathVariable String name) {
-		return name;
-}
-
-@DeleteMapping("/{name}")
-public String deletePlayer(@PathVariable String name) {
-		return "Player " + name + " deleted";
-}
-
-@PutMapping("/{name}")
-public String updatePlayer(@PathVariable String name, @RequestBody String newName) {
-		return "Player " + name + " updated to " + newName;
-}
